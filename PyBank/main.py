fe0c1ca601202449a75e508c306a5a6c30e31256
MarcyVampQueen <savannahcordry@gmail.com>
2020-10-08 16:25:45 -0700

    now able to read the file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,27 @@
 import os
 import csv
 
+# Read in the file
+csvpath = os.path.join(os.path.dirname(__file__), "Resources", "budget_data.csv")
+print(csvpath)
+with open(csvpath) as csvfile:
 
-print("Financial Analysis\n-------------------------")
-print(f"Total Months: {}")
-print(f"Total: ${}")
-print(f"Average Change: ${}")
-print(f"Greatest Increase in Profits: {} (${})")
-print(f"Greatest Decrease in Profits: {} (${})")
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
+
+# print("Financial Analysis\n-------------------------")
+# print(f"Total Months: {}")
+# print(f"Total: ${}")
+# print(f"Average Change: ${}")
+# print(f"Greatest Increase in Profits: {} (${})")
+# print(f"Greatest Decrease in Profits: {} (${})")
