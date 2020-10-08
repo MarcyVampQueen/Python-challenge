613314957c416431fc696368d8454c645499b81c
MarcyVampQueen <savannahcordry@gmail.com>
2020-10-08 16:34:12 -0700

    Totals added

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,27 +1,26 @@
 import os
 import csv
 
+# Define outputs
+rowCount = 0
+totalPL = 0
+
 # Read in the file
 csvpath = os.path.join(os.path.dirname(__file__), "Resources", "budget_data.csv")
-print(csvpath)
 with open(csvpath) as csvfile:
-
-    # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
-
-    # Read the header row first (skip this step if there is now header)
+    # Skip over the header, but save it jic
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
 
     # Read each row of data after the header
     for row in csvreader:
-        print(row)
+        rowCount += 1
+        totalPL = totalPL + int(row[1])
 
-# print("Financial Analysis\n-------------------------")
-# print(f"Total Months: {}")
-# print(f"Total: ${}")
+print("Financial Analysis\n-------------------------")
+print(f"Total Months: {rowCount}")
+print(f"Total: ${totalPL}")
 # print(f"Average Change: ${}")
 # print(f"Greatest Increase in Profits: {} (${})")
 # print(f"Greatest Decrease in Profits: {} (${})")
