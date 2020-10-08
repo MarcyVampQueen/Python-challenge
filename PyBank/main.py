e29c84bc33d5889b0778ee3036dabd3428f02c49
MarcyVampQueen <savannahcordry@gmail.com>
2020-10-07 21:55:28 -0700

    PyBank started

---
+++ PyBank/main.py
@@ -0,0 +1,10 @@
+import os
+import csv
+
+
+print("Financial Analysis\n-------------------------")
+print(f"Total Months: {}")
+print(f"Total: ${}")
+print(f"Average Change: ${}")
+print(f"Greatest Increase in Profits: {} (${})")
+print(f"Greatest Decrease in Profits: {} (${})")
