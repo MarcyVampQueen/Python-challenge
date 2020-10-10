ea501be3ff1bd7000578140a0a459a5ac7a4cb32
MarcyVampQueen <savannahcordry@gmail.com>
2020-10-10 14:25:20 -0700

    totalVotes working

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,9 @@
 import os
 import csv
 
+# Define outputs
+totalVotes = 0
+
 # Read in the file
 csvpath = os.path.join(os.path.dirname(__file__), "Resources", "election_data.csv")
 with open(csvpath) as csvfile:
@@ -11,4 +14,21 @@ with open(csvpath) as csvfile:
 
     # Read each row of data after the header
     for row in csvreader:
-        print(row)
+        totalVotes += 1
+
+output = ["Election Results\n-------------------------\n",
+     f"Total Votes: {totalVotes}\n-------------------------\n"]
+#     f"Total: ${totalProfit_Loss}\n",
+#     f"Khan: {}% ({})\n",
+#     f"Correy: {}% ({})\n",
+#     f"Li: {}% ({})\n",
+#     f"O'Tooley: {}% ({})\n",
+#     ]
+
+output_path = os.path.join(os.path.dirname(__file__), "analysis", "election_results.txt")
+
+# Open the file and write out each line to it and the terminal
+with open(output_path, 'w') as txtfile:
+    for line in output:
+        #txtfile.write(line)
+        print(line, end='')
