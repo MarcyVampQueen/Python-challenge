0c11cbdb2fe51bb6a33af17b0fd803df2e912666
MarcyVampQueen <savannahcordry@gmail.com>
2020-10-10 15:11:05 -0700

    completed

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,6 +3,9 @@ import csv
 
 # Define outputs
 totalVotes = 0
+oldWin = 0
+winner = ""
+candidates = {}
 
 # Read in the file
 csvpath = os.path.join(os.path.dirname(__file__), "Resources", "election_data.csv")
@@ -12,23 +15,28 @@ with open(csvpath) as csvfile:
     # Skip over the header, but save it jic
     csv_header = next(csvreader)
 
-    # Read each row of data after the header
+    # Read each row of data and count
     for row in csvreader:
         totalVotes += 1
 
-output = ["Election Results\n-------------------------\n",
-     f"Total Votes: {totalVotes}\n-------------------------\n"]
-#     f"Total: ${totalProfit_Loss}\n",
-#     f"Khan: {}% ({})\n",
-#     f"Correy: {}% ({})\n",
-#     f"Li: {}% ({})\n",
-#     f"O'Tooley: {}% ({})\n",
-#     ]
+        if row[2] in candidates.keys():
+            candidates[row[2]] += 1
+        else:
+            candidates[row[2]] = 1
+
+output = ["Election Results\n-------------------------\n", f"Total Votes: {totalVotes}\n-------------------------\n"]
+for name, votes in candidates.items():
+    percentWin = votes/totalVotes
+    output.append(f"{name}: {'{0:.2%}'.format(percentWin)} ({votes})\n")
+    if percentWin > oldWin:
+        winner = name
+    oldWin = percentWin
+output.append(f"-------------------------\nWinner: {winner}\n-------------------------")
 
 output_path = os.path.join(os.path.dirname(__file__), "analysis", "election_results.txt")
 
 # Open the file and write out each line to it and the terminal
 with open(output_path, 'w') as txtfile:
     for line in output:
-        #txtfile.write(line)
+        txtfile.write(line)
         print(line, end='')
